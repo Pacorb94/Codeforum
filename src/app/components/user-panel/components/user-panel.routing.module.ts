a120dcdb01b3a46f0100f050a6b697d3a07def72
Pacorb94 <a@a.com>
2022-01-17 13:42:10 +0100

    CHANGE userPanel.routing.module.ts renamed to user-panel.routing.module.ts

---
+++ src/app/components/user-panel/components/user-panel.routing.module.ts
@@ -0,0 +1,37 @@
+//Como es un módulo debemos importar ngModule
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { CheckUserGuard } from '../../../guards/CheckUser.guard';
+import { MainComponent } from './main/main.component';
+import { AddTopicComponent } from './add-topic/add-topic.component';
+import { EditTopicComponent } from './edit-topic/edit-topic.component';
+import { TopicsListComponent } from './topics-list/topics-list.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
+
+const panelRoutes:Routes=[
+    {
+        path:'panel', component:MainComponent, 
+        canActivate:[CheckUserGuard],
+        //Rutas hijas
+        children:[
+            {path:'', component:TopicsListComponent},
+            {path:'topics-list', component:TopicsListComponent},
+            {path:'add-topic', component:AddTopicComponent},
+            {path:'edit-topic/:id', component:EditTopicComponent},
+            {path:'user-settings', component:UserSettingsComponent}
+        ]
+    }
+];
+
+@NgModule({
+    //Añadimos las rutas
+    imports:[
+        RouterModule.forChild(panelRoutes)
+    ],
+    //Para usarlas debemos exportar la clase
+    exports:[
+        RouterModule
+    ]
+})
+
+export class UserPanelRoutingModule{}
